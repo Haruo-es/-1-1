bc024c79788bb52f8c9c5762041ace983c06496e
minamishibuya <minamishibuya@192.168.3.10>
2020-05-21 00:49:08 +0900

    Test

---
+++ skkadai1/src/skkadai1/Main.java
@@ -0,0 +1,11 @@
+package skkadai1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("唐揚げ");
+		System.out.println("なぜかたまにすごく食べたくなるから");
+	}
+
+}
